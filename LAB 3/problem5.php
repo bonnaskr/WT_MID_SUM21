33e8e326d49ab6fe571166018863f3c14b719f26
bonnaskr <60335158+bonnaskr@users.noreply.github.com>
2021-06-13 17:06:53 +0600

    update

---
+++ LAB 3/problem5.php
@@ -0,0 +1,17 @@
+<?php
+$backColor="green"; $fontColor="red"; $imageWidth="80px"; $imageHeight="80px";
+?>
+
+<html>
+    
+<table>
+
+    <tr>
+        <td><img src="aiub-img.png" width=<?php echo $imageWidth?> height=<?php echo $imageHeight?> /></td>
+        <td><h2>American International University -<span style="background-color:<?php echo $backColor?>; color:<?php echo $fontColor?>">
+                Bangladesh</h2></span>
+        </td>
+    </tr>
+    
+</table>
+</html>
